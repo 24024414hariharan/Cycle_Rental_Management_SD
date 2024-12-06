6be55d0d5440a964d7d981e2a89edef01273ffe6
24024414hariharan <24024414@studentmail.ul.ie>
2024-12-06 23:20:27 +0000

    Update stripeClient.ts

--- payment-service/src/clients/stripeClient.ts
+++ payment-service/src/clients/stripeClient.ts
@@ -1,8 +1,9 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-10-28.acacia",
-  typescript: true,
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+//                           , {
+//   apiVersion: "2024-10-28.acacia",
+//   typescript: true,
+// });
 
 export default stripe;
